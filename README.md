43cded098ad2474175dbbfb2dd5cb82cc3ceb2ca
Andrea Pasquetto <a.pasquetto01@gmail.com>
2021-01-28 22:55:01 +0100

    customization

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 A dynamic menu for X written in C.
 
+> All settings can be modified in [config.h](./config.h).
+
 ## Patches
 
 - [border](https://tools.suckless.org/dmenu/patches/border)
@@ -11,3 +13,17 @@ A dynamic menu for X written in C.
 - [line-height](https://tools.suckless.org/dmenu/patches/line-height)
 - [numbers](https://tools.suckless.org/dmenu/patches/numbers)
 - [password](https://tools.suckless.org/dmenu/patches/password)
+
+## Options
+
+| Option         | Action                 |
+| -------------- | ---------------------- |
+| `-b`           | toggle bottom position |
+| `-bw [number]` | set border width       |
+| `-c`           | toggle center position |
+| `-F`           | toggle fuzzy search    |
+| `-l [number]`  | set grid lines         |
+| `-g [number]`  | set grid columns       |
+| `-h [number]`  | set line height        |
+| `-P`           | toggle password mode   |
+| `-p [string]`  | set custom prompt      |
--- config.h
+++ config.h
@@ -1,32 +1,27 @@
-/* See LICENSE file for copyright and license details. */
-/* Default settings; can be overriden by command line. */
 
-static int topbar = 1;      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 0;    /* -c option; centers dmenu on screen */
-static int fuzzy = 1;       /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
-static int min_width = 500; /* minimum width when centered */
-/* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-    "monospace:size=10"};
-static const char *prompt = NULL; /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
-/*                fg         bg       */
-  [SchemeNorm] = {"#bbbbbb", "#222222"},
-  [SchemeSel] = {"#eeeeee", "#005577"},
-  [SchemeOut] = {"#000000", "#00ffff"},
+/* appearance */
+static const char *fonts[]         = {"JetBrainsMono Nerd Font:size=10"};
+static char worddelimiters[]       = " ";
+static int topbar                  = 1;    /* 0 = bottom menu             */
+static unsigned int border_width   = 0;    /* border width                */
+static int centered                = 0;    /* 1 = centered menu           */
+static int min_width               = 720;  /* min width when centered     */
+static int fuzzy                   = 1;    /* 0 = fuzzy matching disabled */
+static unsigned int lines          = 0;    /* grid lines and columns      */
+static unsigned int columns        = 0;    /* grid lines and columns      */
+static unsigned int lineheight     = 0;    /* menu height                 */
+static const char *prompt          = NULL;
+static unsigned int min_lineheight = 8;    /* menu height                 */
+
+/* colors */
+static char normfgcolor[]     = "#ffffff";
+static char normbgcolor[]     = "#000000";
+static char selfgcolor[]      = "#ffffff";
+static char selbgcolor[]      = "#1976d2";
+static const char *colors[][2] = {
+/*                fg           bg         */
+  [SchemeNorm] = {normfgcolor, normbgcolor},
+  [SchemeSel]  = {selfgcolor,  selbgcolor },
+  [SchemeOut]  = {"#000000",   "#00ffff"  },
 };
-/* -l and -g options; controls number of lines and columns in grid if > 0 */
-static unsigned int lines = 0;
-static unsigned int columns = 0;
-/* -h option; minimum height of a menu line */
-static unsigned int lineheight = 0;
-static unsigned int min_lineheight = 8;
 
-/*
- * Characters not considered part of a word while deleting words
- * for example: " /?\"&[]"
- */
-static const char worddelimiters[] = " ";
-
-/* Size of the window border */
-static unsigned int border_width = 0;
